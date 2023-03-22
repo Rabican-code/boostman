17495219bd8cef4e4d7bf28bb7d6778806820b74
Rabican-code <56071999+Rabican-code@users.noreply.github.com>
2023-03-22 11:55:12 +0530

    Update 2023_03_11_065108_campaign_items.php

--- database/migrations/2023_03_11_065108_campaign_items.php
+++ database/migrations/2023_03_11_065108_campaign_items.php
@@ -13,8 +13,10 @@ return new class extends Migration
     {
         Schema::create('campaign_items', function (Blueprint $table) {
             $table->increments('id');
-            $table->text('label');
+            $table->text('name');
             $table->text('content');
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('updated_at')->nullable();
             $table->timestamp('sent_to_day')->nullable();
         });
     }
